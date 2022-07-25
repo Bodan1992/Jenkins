4baadea4cf2703059aee2b6bfc721020570b4fff
Bodan1992 <bodan1992@gmail.com>
2022-07-25 22:00:39 +0300

    add dotenv

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,33 +1,22 @@
-import pytest
 import os
-
+import pytest
+from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene.support.shared import browser
-from selene import Browser, Config
 from dotenv import load_dotenv
 from demoqa_test.controls import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.browser_name = 'chrome'
-    browser.config.timeout = 3
-    browser.config.window_width = 1920
-    browser.config.window_height = 1920
-    # yield
-    # browser.close()
-
-
-DEFAULT_BROWSER_VERSION = "100.0"
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default='100.0'
+    browser.config.base_url = os.getenv('selene.base_url', 'https://demoqa.com')
+    browser.config.browser_name = os.getenv('selene.browser_name', 'chrome')
+    browser.config.hold_browser_open = (
+            os.getenv('selene.hold_browser_open', 'false').lower() == 'true'
     )
+    browser.config.timeout = float(os.getenv('selene.timeout', '3'))
+    browser.config.window_width = 1600
+    browser.config.window_height = 800
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -36,13 +25,11 @@ def load_env():
 
 
 @pytest.fixture(scope='function')
-def setup_browser(request):
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+def setup_browser():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": browser_version,
+        "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
@@ -57,8 +44,7 @@ def setup_browser(request):
         command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
-    browser = Browser(Config(driver))
-
+    browser.config.driver = driver
     yield browser
 
     attach.add_html(browser)
@@ -66,3 +52,4 @@ def setup_browser(request):
     attach.add_logs(browser)
     attach.add_video(browser)
     browser.quit()
+

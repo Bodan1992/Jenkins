75b49ba539750ae6ec831054dff4efa3724438b0
Bodan1992 <bodan1992@gmail.com>
2022-07-25 22:49:05 +0300

    fix conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
 from demoqa_test.controls import attach
+from selene import Browser, Config
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -19,17 +20,29 @@ def browser_management():
     browser.config.window_height = 1920
 
 
+DEFAULT_BROWSER_VERSION = "100.0"
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+
 @pytest.fixture(scope='session', autouse=True)
 def load_env():
     load_dotenv()
 
 
 @pytest.fixture(scope='function')
-def setup_browser():
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
@@ -44,7 +57,8 @@ def setup_browser():
         command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
-    browser.config.driver = driver
+    browser = Browser(Config(driver))
+
     yield browser
 
     attach.add_html(browser)
@@ -52,4 +66,3 @@ def setup_browser():
     attach.add_logs(browser)
     attach.add_video(browser)
     browser.quit()
-
